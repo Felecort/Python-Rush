b3f353f762daf123dcee247ef87ed6f9780b5933
FriLDD <dotpointen@gmail.com>
2021-09-25 18:12:54 +0300

    refactor

--- src/mini_projects/xlsx2txt_reword.py
+++ src/mini_projects/xlsx2txt_reword.py
@@ -1,31 +1,28 @@
 import openpyxl
 
-xls_parth = "D:\\Projects\\PythonProjects\\Learn\\words.xlsx"
-txt_parth_default = "D:\\Projects\\PythonProjects\\Learn\\words.txt"
+xls_parth = r"D:\Projects\PythonProjects\Learn\words.xlsx"
+txt_parth = r"D:\Projects\PythonProjects\Learn\words.txt"
 wookbook = openpyxl.load_workbook(xls_parth)
 worksheet = wookbook.active
 
 
-def write_in_file(text):
-    with open(txt_parth_default, "w", encoding="UTF-8") as file:
-        file.write(text)
-        
-
-def pars_default_words():
-    rows = worksheet.max_row
-    i = 3
-    text = ""
-    while True:
-        row_arr = []
-        for j in range(2, 4):
-            cell = worksheet.cell(row=i, column=j).value
-            if not cell:
-                write_in_file(text)
-                return
-            row_arr.append(cell)
-        text += f'\"{row_arr[0]}\";\"{row_arr[1]}\"\n'
-        row_arr.clear()
-        i += 1
+def write_in_file(row_arr, file):
+    text = f'\"{row_arr[0]}\";\"{row_arr[1]}\"\n'
+    file.write(text)
 
 
-pars_default_words()
+def parse_default_words():
+    row_num = 3
+    with open(txt_parth, "w", encoding="UTF-8") as file:
+        while True:
+            row_arr = []
+            for col_num in range(2, 4):
+                cell = worksheet.cell(row=row_num, column=col_num).value
+                if not cell:
+                    return
+                row_arr.append(cell)
+            write_in_file(row_arr, file)        
+            row_num += 1
+
+
+parse_default_words()
