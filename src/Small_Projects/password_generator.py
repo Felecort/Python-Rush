d299db84f25400d4914a848091a946c8151d9fc5
FriLDD <dotpointen@gmail.com>
2021-10-22 18:56:22 +0300

    Improved file structure

--- src/Small_Projects/password_generator.py
+++ src/Small_Projects/password_generator.py
@@ -2,5 +2,5 @@
 from string import ascii_letters, digits
 from random import randrange, choice
 all_chars = ascii_letters + digits
-password = "".join([choice(all_chars) for _ in range(randrange(15, 20))])
+password = "".join([choice(all_chars) for _ in range(randrange(20, 25))])
 print(password)
