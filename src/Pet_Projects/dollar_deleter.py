a1057ceb92e04074a129cfe89018a55952a2381e
FriLDD <dotpointen@gmail.com>
2022-01-07 20:49:22 +0300

    Okey.... I did it today... Ehh...

--- src/Pet_Projects/dollar_deleter.py
+++ src/Pet_Projects/dollar_deleter.py
@@ -1,6 +1,16 @@
-from typing import Pattern
+
+output_file_path = r"D:\Projects\PythonProjects\Python-rush\src\tmp_files\liza_text_out.txt"
+path_to_file = r"D:\Projects\PythonProjects\Python-rush\src\tmp_files\liza_text.txt"
+
+with open(path_to_file, "r", encoding="utf-8") as data:
+    text = data.read()
+    output_text = []
+    append_output_text = output_text.append
+    for char in text:
+        if char != "$":
+            append_output_text(char)
+    output_text = "".join(output_text)
 
 
-path_to_file = ...
-with open(path_to_file, "r") as text:
-    ...
+with open(output_file_path, "w", encoding="utf-8") as out_file:
+    out_file.write(output_text)
