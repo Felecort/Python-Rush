8990bf12467cd507506b72f3627692f45f40faeb
FriLDD <dotpointen@gmail.com>
2022-01-12 23:00:36 +0300

    pep 8 refactor

--- src/code_visualization/py_heat_example.py
+++ src/code_visualization/py_heat_example.py
@@ -1,5 +1,5 @@
 from pyheat import PyHeat
-
-ph = PyHeat(r'D:\Projects\PythonProjects\Python-rush\src\Algorithms\sort\selection_sort.py')
+path = r'D:\Projects\PythonProjects\Python-rush\src\Algorithms\sort\selection_sort.py'
+ph = PyHeat(path)
 ph.create_heatmap()
 ph.show_heatmap()
