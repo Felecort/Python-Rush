cf56a9bc38fb2f5eb89a62001819ccfb7ca8a5d7
Felecort <dotpointen@gmail.com>
2022-03-21 19:00:36 +0300

    unnecessary files deleted

--- src/Practical_Tasks/open_web_page.py
+++
@@ -1,9 +0,0 @@
-from os import system, getcwd
-
-if __name__ == "__main__":
-    root_dir = getcwd()
-    with open(f'{root_dir}\\site_list.txt', 'r') as src_list:
-        for line in src_list.readlines():
-            print(line[:-1])
-            system(f"powershell -C Start-Process chrome.exe -ArgumentList @('-incognito','{line[:-1]}')")
-
--- src/Practical_Tasks/password_generator.py
+++
@@ -1,9 +0,0 @@
-
-from string import ascii_letters, digits
-from random import randrange, choice
-
-if __name__ == "__maon__":
-    all_chars = ascii_letters + digits
-    password = "".join([choice(all_chars)
-                        for _ in range(randrange(20, 25))])
-    print(password)
