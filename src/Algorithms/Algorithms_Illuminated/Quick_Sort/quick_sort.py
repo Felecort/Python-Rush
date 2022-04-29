d801e98d4fbc334f993ed8691c2aafcb3c076fbb
Felecort <dotpointen@gmail.com>
2022-04-30 00:29:46 +0300

    added much pleasent api for quick sort

--- src/Algorithms/Algorithms_Illuminated/Quick_Sort/quick_sort.py
+++ src/Algorithms/Algorithms_Illuminated/Quick_Sort/quick_sort.py
@@ -17,18 +17,22 @@ def partition(arr, l, r):
     return (i - 1)
 
 
-def quick_sort(arr, l, r):
+def quick_sort_(arr, l, r):
     if l >= r:
         return 
     i = choose_pivot(arr, l, r)
     arr[l], arr[i] = arr[i], arr[l]
     j = partition(arr, l, r)
-    quick_sort(arr, l, j - 1)
-    quick_sort(arr, j + 1, r)
+    quick_sort_(arr, l, j - 1)
+    quick_sort_(arr, j + 1, r)
     
-    
+
+def quick_sort(arr):
+    quick_sort_(arr, ..., ...)
+
+
 if __name__ == "__main__":
     arr = [random.randint(1, 30) for _ in range(20)]
     print(arr)
-    quick_sort(arr, ..., ...)
+    quick_sort(arr)
     print(arr)
