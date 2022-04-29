32c69b57e30d723426e6d92a8caa8e1223aec65c
FriLDD <dotpointen@gmail.com>
2022-04-29 19:56:50 +0300

    added other version partition func

--- src/Algorithms/Algorithms_Illuminated/Quick_Sort/partition.py
+++ src/Algorithms/Algorithms_Illuminated/Quick_Sort/partition.py
@@ -1,6 +1,18 @@
 from random import randint
 
 
+def partition_(arr, l, r):
+    p = arr[l]
+    i = l + 1
+    for j in range(l + 1, r + 1):
+        if arr[j] < p:
+            arr[j], arr[i] = arr[i], arr[j]
+            i += 1
+    arr[l], arr[i - 1] = arr[i - 1], arr[l]
+    
+    return (i - 1)
+
+
 def partition(arr):
     left = 1
     for right in range(1, len(arr)):
@@ -12,10 +24,17 @@ def partition(arr):
 
 
 if __name__ == "__main__":
-    for _ in range(1_000_000):
-        arr = [randint(1, 30) for _ in range(10)]
-        arr_copy = arr[:]
-        left = partition(arr)
+    arr = [randint(1, 30) for _ in range(20)]
+    i, j = 5, 15
+    print(arr[:i], arr[i:])
+    left = partition_(arr, 5, 15)
+    print(arr)
+    
+    
+    # for _ in range(1_000_000):
+    #     arr = [randint(1, 30) for _ in range(10)]
+    #     arr_copy = arr[:]
+    #     left = partition(arr)
 
-        if not all([arr[i] <= (arr[left]) for i in range(0, left)]):
-            print(arr_copy, arr, left)
+    #     if not all([arr[i] <= (arr[left]) for i in range(0, left)]):
+    #         print(arr_copy, arr, left)
